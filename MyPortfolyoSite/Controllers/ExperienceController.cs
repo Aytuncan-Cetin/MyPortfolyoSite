75370a5e0dca2e673d2f7c449e56d37fd7befffe
Aytuncan-Cetin <aytuncancetin@hotmail.com>
2025-03-01 23:41:10 +0300

    Admin Paneli Icin Layout controller ve view in olusturulmasi

---
+++ MyPortfolyoSite/Controllers/ExperienceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolyoSite.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult ExperienceList()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortfolyoSite/Controllers/LayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolyoSite.Controllers
+{
+    public class LayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
