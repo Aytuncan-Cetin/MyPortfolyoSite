cab34f24f56981bd7db91d6d304fca64e359d7fa
Aytuncan-Cetin <aytuncancetin@hotmail.com>
2025-03-02 00:50:15 +0300

    Admin Panelindeki Layout kismindeki butun partialler duzenlendi

---
+++ MyPortfolyoSite/ViewComponents/LayoutViewComponent/_LayoutSideBarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolyoSite.ViewComponents.LayoutViewComponent
+{
+    public class _LayoutSideBarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
