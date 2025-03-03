6bc72aff0194e60a4ca05986aeec891a2b86b659
Aytuncan-Cetin <aytuncancetin@hotmail.com>
2025-03-03 23:51:57 +0300

    ExperienceList olusturuldu duzenlendi

--- MyPortfolyoSite/Controllers/ExperienceController.cs
+++ MyPortfolyoSite/Controllers/ExperienceController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolyoSite.DAL.Context;
 
 namespace MyPortfolyoSite.Controllers
 {
     public class ExperienceController : Controller
     {
+        MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult ExperienceList()
         {
-            return View();
+            var values = context.Experiences.ToList();
+            return View(values);
         }
     }
 }
